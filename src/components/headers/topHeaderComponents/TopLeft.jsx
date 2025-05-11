28233c6600f97dd5ddfa0a22a24403e08a7077e7
Your Name <fahimahmad123go@gmail.com>
2025-05-12 00:49:27 +0600

    hover effect added
    hover:text-[#FF624C] transition-all duration-150 ease-in-out

--- src/components/headers/topHeaderComponents/TopLeft.jsx
+++ src/components/headers/topHeaderComponents/TopLeft.jsx
@@ -6,14 +6,14 @@ import { Link } from "react-router-dom";
 const TopLeft = () => {
   return (
     <div className="flex items-center gap-6 ">
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 hover:text-[#FF624C] transition-all duration-150 ease-in-out">
         <TfiLocationPin className="text-base" />
         <Link to={"https://maps.app.goo.gl/Q2Sc2SEXxMzaRDRA9"} target="_blank">
         123 Main Street, AnyTown USA
       </Link>
       </div>
       <span className="w-[1px] h-8 bg-[#CBCBCB] "></span>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 hover:text-[#FF624C] transition-all duration-150 ease-in-out">
         <LiaPhoneSolid className="text-lg" />
       <Link to={"tel:1613529163"} target="_blank">
         +1 (555) 123-4567
