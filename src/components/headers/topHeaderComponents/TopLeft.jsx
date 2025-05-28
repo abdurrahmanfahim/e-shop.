f0d5a96556b440bd138dc52e2a628a47c9918273
Your Name <fahimahmad123go@gmail.com>
2025-05-28 17:37:36 +0600

    Update TopLeft.jsx

--- src/components/headers/topHeaderComponents/TopLeft.jsx
+++ src/components/headers/topHeaderComponents/TopLeft.jsx
@@ -6,16 +6,16 @@ import PhoneHeader from "../../../icons/PhoneHeader";
 const TopLeft = () => {
   return (
     <div className="flex items-center gap-6 ">
-      <div className="flex items-center gap-2 hover:text-[#FF624C] transition-all duration-150 ease-in-out">
+      <div>
+        <Link className="flex items-center gap-2 hover:text-[#FF624C] transition-all duration-150 ease-in-out" to={"https://maps.app.goo.gl/Q2Sc2SEXxMzaRDRA9"} target="_blank">
         <LocationHeader />
-        <Link to={"https://maps.app.goo.gl/Q2Sc2SEXxMzaRDRA9"} target="_blank">
         123 Main Street, AnyTown USA
       </Link>
       </div>
       <span className="w-[1px] h-8 bg-[#CBCBCB] "></span>
-      <div className="flex items-center gap-2 hover:text-[#FF624C] transition-all duration-150 ease-in-out">
+      <div>
+      <Link className="flex items-center gap-2 hover:text-[#FF624C] transition-all duration-150 ease-in-out" to={"tel:1613529163"} target="_blank">
         <PhoneHeader />
-      <Link to={"tel:1613529163"} target="_blank">
         +1 (555) 123-4567
       </Link>
       </div>
