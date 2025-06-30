bc21714b3b19f2c1e596826ef2f8fb4dd4a40148
Your Name <fahimahmad123go@gmail.com>
2025-06-30 23:29:25 +0600

    Revamp pagination logic for improved page display
    Refactored the pagination component to show a maximum of 5 pages with ellipsis for skipped ranges. This improves usability for large datasets and simplifies the page navigation logic.

--- src/components/layouts/Pagination.jsx
+++ src/components/layouts/Pagination.jsx
@@ -9,23 +9,47 @@ const Pagination = ({
   onPageChange,
 }) => {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
-  const maxPagesToShow = 10;
-  const pagesToShowAfterBefore = 3;
+  const maxPagesToShow = 5;
+  const pages = [];
 
-  let startPage = Math.max(1, currentPage - pagesToShowAfterBefore);
-  let endPage = Math.max(totalPages, currentPage + pagesToShowAfterBefore);
-
-  if (endPage - startPage + 1 < maxPagesToShow) {
-    if (startPage > 1) {
-      startPage = Math.max(1, endPage - maxPagesToShow + 1);
+  if (totalPages <= maxPagesToShow) {
+    for (let i = 0; i <= totalPages; i++) {
+      pages.push(i);
     }
-    endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
+  } else {
+    const startPages = [1, 2, 3];
+    const endPages = [totalPages-1, totalPages];
+    const middlePages = [currentPage - 1, currentPage, currentPage + 1].filter(
+      (p) => p > 3 && p < totalPages - 1
+    );
+
+    const uniquePages = Array.from(new Set([...startPages, ...middlePages , ...endPages])).sort((a, b) => a - b)
+
+    for (let i = 0; i < uniquePages.length - 1; i++) {
+
+      pages.push(uniquePages[i])
+      
+      if (i < uniquePages.length - 1 && uniquePages[i + 1] - uniquePages[1] > 1) {
+        pages.push("...")
+      }
+    }
+
   }
 
-  const pageNumber = [];
-  for (let i = startPage; i <= endPage; i++) {
-    pageNumber.push(i);
-  }
+  // let startPage = Math.max(1, currentPage - pagesToShowAfterBefore);
+  // let endPage = Math.max(totalPages, currentPage + pagesToShowAfterBefore);
+
+  // if (endPage - startPage + 1 < maxPagesToShow) {
+  //   if (startPage > 1) {
+  //     startPage = Math.max(1, endPage - maxPagesToShow + 1);
+  //   }
+  //   endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
+  // }
+
+  // const pageNumber = [];
+  // for (let i = startPage; i <= endPage; i++) {
+  //   pageNumber.push(i);
+  // }
 
   return (
     <div className="w-[522px] mx-auto flex gap-[18px] items-center justify-center p-4 font-['Poppins'] font-semibold text-xl leading-[30px] text-center text-[#303030] ">
@@ -37,7 +61,7 @@ const Pagination = ({
         <PrevIcon />
       </button>
 
-      {pageNumber.map((number, index) => (
+      {pages.map((number, index) => (
         <div
           className="w-full flex justify-center items-center  "
           key={index * index * 7}
